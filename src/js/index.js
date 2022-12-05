f94509ce49cae909172c56eb59936c7224fc37c3
Anna <stepanova.ao@gmail.com>
2022-12-05 20:21:56 +0300

    add slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,23 @@
 import '../styles/style.scss';
 import burger from './modules/burger';
+import Swiper from 'swiper';
+import 'swiper/css';
+
 
 window.addEventListener('DOMContentLoaded', () => {
    burger('.header__burger');
+   const swiper = new Swiper('.swiper', {
+      loop: true,
+      spaceBetween: 23,
+      centeredSlides: true,
+      slidesPerView: 3,
+      breakpoints: {
+         0: {
+            slidesPerView: 1
+         },
+         797.98: {
+            slidesPerView: 3,
+         }
+      }
+  });
 });
